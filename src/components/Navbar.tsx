40aa922dd9a82706b30f7ebc0fd9a9b31b200d0c
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-08-27 13:27:24 +0000

    Apply motion framer animation for mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,23 +3,23 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="hidden md:flex items-center gap-8">
-      <ul className="list-none flex flex-row justify-center items-center gap-8 text-xl uppercase">
-        <li className="px-7">
+      <ul className="list-none flex flex-row justify-center items-center gap-8 text-xl uppercase cursor-pointer">
+        <li className="px-7 hover:text-hover-effect">
           <Link to="/">
             <span>Home</span>
           </Link>
         </li>
-        <li className="px-7">
+        <li className="px-7 hover:text-hover-effect">
           <Link to="/">
             <span>About</span>
           </Link>
         </li>
-        <li className="px-7">
+        <li className="px-7  hover:text-hover-effect">
           <Link to="/">
             <span>Contact</span>
           </Link>
         </li>
-        <li className="px-7">
+        <li className="px-7 hover:text-hover-effect">
           <Link to="/">
             <span>Shop</span>
           </Link>
--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,45 +1,49 @@
 import { Link } from "react-router-dom";
 import Navbar from "../Navbar";
-// import logo from "/assets/icons/rehoboth-logo.png";
 import menu from "/assets/icons/menu.png";
+import MobileNavbar from "./MobileNavbar";
+import { useState } from "react";
 
 const HeroSection = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
   return (
-    <section className="flex flex-col w-full px-10 xl:px-30 py-5 bg-[url(/assets/Fresh-Flower/fresh-flower14.jpg)] bg-no-repeat bg-cover min-h-screen md:min-h-[100vh] bg-center mg:bg-size[100%]">
+    <section className="flex flex-col w-full px-10 xl:px-30 py-5 bg-[url(/assets/Fresh-Flower/fresh-flower13.jpg)] bg-no-repeat bg-cover min-h-screen md:min-h-[100vh] bg-center mg:bg-size[100%]">
       <header className="flex flex-row  items-center justify-between gap-10">
         <div className="logo-container">
-          {/* <img
-            src={logo}
-            alt="rehoboth logo"
-            className="w-30 h-25 rounded-full"
-          /> */}
-          <h1 className="text-2xl italic">Rohoboth Floral</h1>
+          <h1 className="text-3xl italic md:text-4xl text-[#f83600] ">
+            Rohoboth Floral
+          </h1>
         </div>
-        <div className="flex flex-row justify-center gap-8  px-4 py-5 bg-white/20 backdrop-blur-md">
+        <div className="flex flex-row justify-center gap-8 md:p-2 bg-white/20 backdrop-blur-md">
           <div className="hidden md:flex gap-8">
             <Navbar />
           </div>
-          <button className="border-none">
+          <button
+            className="border-none hover: bg-white/20 p-3 rounded-xl cursor-pointer"
+            onClick={() => setShowNavbar((prev) => !prev)}
+          >
             <img src={menu} alt="menu-icon" className="w-8" />
           </button>
         </div>
       </header>
-
-      <div className="text-light-cream space-y-4 text-center my-10 flex flex-col flex-1 justify-center items-center">
-        <p className="text-2xl text-bold tracking-wide">
+      <div className="space-y-6 text-center my-12 flex flex-col flex-1 justify-center items-center px-4  text-white">
+        <p className="text-2xl md:text-3xl font-semibold tracking-wider text-white">
           Rohoboth Florals Shop
         </p>
-        <h1 className="text-5xl md:text-6xl font-bold leading-tight">
+        <h1 className="text-5xl md:text-6xl font-extrabold leading-tight drop-shadow-md">
           A Perfect Bouquet for Every Occasion
         </h1>
-        <p className="md:text-lg max-w-xl">
-          Discover the beauty of fresh flowers, handcrafted with love to bring
-          joy, celebrate milestones, and make every moment unforgettable.
+        <p className="text-lg md:text-xl max-w-2xl font-medium leading-relaxed">
+          Discover the beauty of{" "}
+          <span className="font-semibold">fresh flowers</span>, handcrafted with
+          love to bring joy, celebrate milestones, and make every moment
+          unforgettable.
         </p>
-        <button className="text-xl mt-4 bg-gradient-to-r from-[#ff7e28] to-[#e63900] hover:bg-hover-effect text-white font-semibold px-8 py-3 rounded-xl transition duration-300">
+        <button className="text-lg md:text-xl mt-6 bg-gradient-to-r from-[#f83600] to-[#facc22] text-white font-semibold px-10 py-4 rounded-2xl shadow-lg hover:scale-105 hover:shadow-xl transition-transform duration-300">
           <Link to="/shop">Shop Now</Link>
         </button>
       </div>
+      <MobileNavbar showNavbar={showNavbar} />
     </section>
   );
 };
