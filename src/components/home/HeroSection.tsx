8ea5f0343678cbb9be0fce836d258524de2226ff
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-08-28 13:33:59 +0000

    Apply wrapper to the hero section

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -8,9 +8,10 @@ const HeroSection = () => {
   const [showNavbar, setShowNavbar] = useState(false);
   return (
     <section className="flex flex-col w-full px-10 xl:px-30 py-5 bg-[url(/assets/arrangements-in-vases-boxes/flower12.jpeg)] bg-no-repeat bg-cover min-h-screen md:min-h-[100vh] bg-center mg:bg-size[100%]">
+      <div className="hero-overlay"></div>
       <header className="flex flex-row  items-center justify-between gap-10">
         <div className="logo-container">
-          <h1 className="text-3xl italic md:text-4xl text-[#f83600] ">
+          <h1 className="text-3xl italic md:text-4xl text-[#f83600] drop-shadow-md">
             Rehoboth Floral
           </h1>
         </div>
@@ -26,20 +27,21 @@ const HeroSection = () => {
           </button>
         </div>
       </header>
+
       <div className="space-y-6 text-center my-12 flex flex-col flex-1 justify-center items-center px-4  text-white">
-        <p className="text-2xl md:text-3xl font-semibold tracking-wider text-white">
+        <p className="text-2xl md:text-3xl font-semibold tracking-wider text-white drop-shadow-md">
           Rehoboth Florals Shop
         </p>
         <h1 className="text-5xl md:text-6xl font-extrabold leading-tight drop-shadow-md">
           A Perfect Bouquet for Every Occasion
         </h1>
-        <p className="text-lg md:text-xl max-w-2xl font-medium leading-relaxed">
+        <p className="text-lg md:text-xl max-w-2xl font-medium leading-relaxed  text-white drop-shadow-md">
           Discover the beauty of{" "}
           <span className="font-semibold">fresh flowers</span>, handcrafted with
           love to bring joy, celebrate milestones, and make every moment
           unforgettable.
         </p>
-        <button className="text-lg md:text-xl mt-6 bg-gradient-to-r from-[#f83600] to-[#facc22] text-white font-semibold px-10 py-4 rounded-2xl shadow-lg hover:scale-105 hover:shadow-xl transition-transform duration-300">
+        <button className="text-lg md:text-xl mt-6 bg-gradient-to-r from-[#f83600] to-[#facc22] text-white drop-shadow-md font-semibold px-10 py-4 rounded-2xl shadow-lg hover:scale-105 hover:shadow-xl transition-transform duration-300">
           <Link to="/shop">Shop Now</Link>
         </button>
       </div>
