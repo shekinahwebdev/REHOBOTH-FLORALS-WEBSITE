6b51a37a6281d05b66827842b8e4cfa2a8fd2067
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-08-29 13:48:27 +0000

    Implement product section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "../components/home/HeroSection";
 import IntroductionSection from "../components/home/IntroductionSection";
+import Product from "../components/home/Product";
 
 const Home = () => {
   return (
     <section className="homepage">
       <HeroSection />
       <IntroductionSection />
+      <Product />
     </section>
   );
 };
