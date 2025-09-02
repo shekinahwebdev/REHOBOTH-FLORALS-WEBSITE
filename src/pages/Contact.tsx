38354a24a195e04a9e0d346b5e56d59c9b92993d
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-09-02 23:11:08 +0000

    Change contact page background image

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,31 +1,32 @@
 import { Phone } from "lucide-react";
-import ContactCard from "../components/contact/contactCard";
 import ContactForm from "../components/contact/ContactForm";
 import Services from "../components/contact/Services";
 import ShopLocation from "../components/contact/ShopLocation";
+import ContactCard from "../components/contact/ContactCard";
 
 const Contact = () => {
   return (
-    <div className="pt-16 bg-white min-h-screen">
-      <section className=" bg-light-cream py-20 mt-7">
+    <div className="pt-16 min-h-screen">
+      <section className="bg-[url(/assets/bouquets/bouquet-flower7.jpeg)] bg-no-repeat bg-cover bg-center mg:bg-size[100%] overflow-hidden py-20 relative">
+        <div className="contact-overlay"></div>
         <div className="mmx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <h1 className="text-4xl md:text-5xl font-bold text-dark-charcoal mb-4">
+          <h1 className="text-4xl md:text-5xl font-bold text-white mb-4 drop-shadow-md">
             Get In Touch
           </h1>
-          <p className="text-xl text-warm-gray max-w-2xl mx-auto">
+          <p className="text-xl  text-white max-w-2xl mx-auto drop-shadow-md">
             We'd love to help you create the perfect floral arrangement for your
             special occasion.
           </p>
         </div>
       </section>
-      <section className="py-16 px-5">
+      <section className="py-16 px-5 bg-light-cream">
         <div className="px-4 sm:px-6 lg:px-8">
           <ContactCard />
         </div>
       </section>
-
-      <section className="py-16 bg-light-cream">
-        <div className="md:max-w-[100rem] md:mx-auto px-4 sm:px-6 lg:px-8">
+      <section className="py-16 bg-[url(/assets/arrangements-in-vases-boxes/flower24.jpeg)] bg-no-repeat bg-cover bg-center mg:bg-size[100%] overflow-hidden relative">
+        <div className="form-overlay"></div>
+        <div className="md:max-w-[100rem] md:mx-auto px-4 sm:px-6 lg:px-8 drop-shadow-md">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
             <ContactForm />
             <div className="space-y-8">
@@ -35,12 +36,12 @@ const Contact = () => {
           </div>
         </div>
       </section>
-      <section className="py-16 md:py-20 bg-slight-background-tint">
+      <section className="py-16 md:py-20 bg-gradient-to-r from-[#facc22] to-[#f83600] ">
         <div className="max-w-4xl mx-auto text-center px-4 sm:px-6 lg:px-8">
           <h2 className="text-2xl md:text-3xl font-bold text-dark-charcoal mb-4">
             Need Last-Minute Arrangements?
           </h2>
-          <p className="text-warm-gray mb-6 md:text-2xl">
+          <p className="text-white mb-6 md:text-2xl">
             We understand that sometimes special occasions arise unexpectedly.
             Call us for same-day service and emergency arrangements.
           </p>
